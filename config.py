2389d9115fa79eb05e619d36130b9fb649ca98b7
AlanSureshJ <147090209+AlanSureshJ@users.noreply.github.com>
2025-07-13 20:22:40 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'root'
-    MYSQL_DB = 'BOOKBANK'
